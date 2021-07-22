e80f9641ea72555f5691669ce3e1502e6a7e259d
Lucas de Almeida <lucasdealmeida.monteiro@gmail.com>
2021-07-22 11:38:31 -0300

    Adding connection to the database via MongoDB, the functionality to search for a specific product in the API and the customization of the GetProducts/index.js file was also added.

---
+++ GetProductById/index.js
@@ -0,0 +1,25 @@
+const ObjectId = require('mongodb').ObjectId;
+const createMongoClient = require('../shared/mongoClient');
+
+module.exports = async function (context, req) {
+    const { id } = req.params;
+
+    if (!id) {
+        context.res = {
+            status: 400,
+            body: 'Provide a product id on params',
+        };
+        return;
+    }
+
+    const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+    const Products = MongoClient.collection('products');
+    const body = await Products.findOne({ '_id': ObjectId(id) });
+
+    closeConnectionFn();
+
+    context.res = {
+        status: 200,
+        body
+    };
+};
